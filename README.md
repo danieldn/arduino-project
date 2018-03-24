e35a42e2170410abb5add4ad5738b7880d53014a
Daniel Nguyen <daniel.nguyen@intel.com>
2018-03-24 00:44:39 -0700

    Fetch local weather data
    Enables arduino to fetch local weather data from via Yahoo
    Weather RSS Feed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# arduino-project
+# Paladin
+
+Arduino project for IoT Hackathon 2018 by Women Who Code (Portland) at Puppet.
+\#WWCodeHack18
+
+## Setup
+
+Arduino IDE 1.8+
+Arduino 101
+
+
---
+++ localWeather/TembooAccount.h
@@ -0,0 +1,13 @@
+#define TEMBOO_ACCOUNT "paladin"  
+#define TEMBOO_APP_KEY_NAME "myFirstApp"
+#define TEMBOO_APP_KEY "PoPlUSpy0KfEEuVN10GUuiMjrH08pbh9"  
+#define TEMBOO_DEVICE_TYPE "a101+w101"
+
+#define WIFI_SSID "Puppet Guest"
+#define WPA_PASSWORD "argon4949"
+
+#if TEMBOO_LIBRARY_VERSION < 2
+#error "Your Temboo library is not up to date." 
+#endif
+
+
