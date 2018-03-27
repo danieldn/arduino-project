5b41b1ae191ac29a514f0fc717b9c2e2f253ce26
jastr945 <polina.jastr@gmail.com>
2018-03-26 20:41:22 -0700

    Added a logo to the navbar

--- webapp/static/lights.js
+++ webapp/static/lights.js
@@ -1,22 +1,22 @@
-"use strict";
-
-function get_lights(){
-  console.log("working");
-    var feedback = $.ajax({
-        type: "GET",
-        url: "/lights",
-        async: false
-    }).done(function(data){
-      if (data === "1") {
-        console.log("return 1");
-        $('#bulb').addClass('yellow');
-        $('#commentBox').addClass('light');
-      } else {
-        console.log("return 0");
-        $('#bulb').removeClass('yellow');
-        $('#commentBox').removeClass('light');
-      }
-    });
-}
-
-setInterval(get_lights, 1000);
+// "use strict";
+//
+// function get_lights(){
+//   console.log("working");
+//     var feedback = $.ajax({
+//         type: "GET",
+//         url: "/lights",
+//         async: false
+//     }).done(function(data){
+//       if (data === "1") {
+//         console.log("return 1");
+//         $('#bulb').addClass('yellow');
+//         $('#commentBox').addClass('light');
+//       } else {
+//         console.log("return 0");
+//         $('#bulb').removeClass('yellow');
+//         $('#commentBox').removeClass('light');
+//       }
+//     });
+// }
+//
+// setInterval(get_lights, 1000);
