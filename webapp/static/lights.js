77f7745a63b40441959cf3912f7a9acc1cd13282
jastr945 <polina.jastr@gmail.com>
2018-03-29 14:24:59 -0700

    Added js functions on change of the switch button, added more info to the footer

--- webapp/static/lights.js
+++ webapp/static/lights.js
@@ -1,18 +1,37 @@
 "use strict";
 
+// Sending requests to Flask to turn the motion sensor on or off
+$( document ).ready(function() {
+  $('.checkbox').change(function() {
+      if($(this).is(':checked')) {
+        $.ajax({
+          type: "GET",
+          url: "/on",
+          async: false
+        });
+      } else {
+        $.ajax({
+          type: "GET",
+          url: "/off",
+          async: false
+        });
+      }
+  });
+});
+
+// Grabbing the current state every second and changing the colors on the website accordingly
 function get_lights(){
-  console.log("working");
     var feedback = $.ajax({
         type: "GET",
         url: "/lights",
         async: false
     }).done(function(data){
       if (data === "1") {
-        console.log("return 1");
+        // console.log("return 1");
         $('#bulb').addClass('bulb-on').removeClass('bulb-off');
         $('body').addClass('light');
       } else {
-        console.log("return 0");
+        // console.log("return 0");
         $('#bulb').removeClass('bulb-on').addClass('bulb-off');
         $('body').removeClass('light');
       }
